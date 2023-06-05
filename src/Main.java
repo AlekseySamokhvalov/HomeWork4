1a448c5ce125844fa84c71e70b674df0258dc4fb
AlekseySamokhvalov <6560@mail.ru>
2023-06-06 01:30:29 +0300

    Переназначаем репозиторий.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task1(); //вызов метода решения задачи 1
         task2();
         task3();
         task4();
