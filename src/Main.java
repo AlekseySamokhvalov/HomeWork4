304a7a9b8eb21f8921d678138d28feae2f19e019
AlekseySamokhvalov <6560@mail.ru>
2023-06-06 02:34:45 +0300

    Решение задачи 2.

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,18 @@ public class Main {
     public static void task2 () {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите температуру воздуха: ");
+        int temperature = scanner.nextInt();
+
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        }
+        if(temperature >= 5) {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+
     }
 
     public static void task3 () {
